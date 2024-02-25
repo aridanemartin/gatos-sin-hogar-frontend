021caffb4c1c5cf5f34931568cb8b1bb26ad9257
Kilian Sosa Guillén <kilianjonayyairam@gmail.com>
2024-02-25 12:13:49 +0000

    fix: API endpoint in AddLocationForm

--- src/components/AddLocationForm/AddLocationForm.tsx
+++ src/components/AddLocationForm/AddLocationForm.tsx
@@ -47,7 +47,7 @@ export const AddLocationForm = ({ coords }: { coords: LatLngLiteral }) => {
     try {
       const validatedData = CatLocationFormSchema.parse(selectedLocation);
 
-      await fetch(`${baseUrl}/locations/`, {
+      await fetch(`${baseUrl}/locations`, {
         method: 'POST',
         headers: {
           'Content-Type': 'application/json'
