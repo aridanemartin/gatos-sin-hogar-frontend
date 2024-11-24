f8cdbd5a9158f11b703af24776af98d35585d231
Aridane Martín <aridanemartin@gmail.com>
2024-11-24 20:38:39 +0000

    feat: style improvements

---
+++ src/layouts/MainLayout.tsx
@@ -0,0 +1,67 @@
+import { Link, Outlet } from 'react-router-dom';
+import './MainLayout.scss';
+import gatosSinHogarNavLogo from '@assets/logos/navLogo.png';
+import { Button, ButtonType } from '@components/Button/Button';
+
+const MainLayout = () => {
+  return (
+    <div>
+      <header>
+        <nav className="mainLayout__nav">
+          <img src={gatosSinHogarNavLogo} alt="" height={55} />
+          <div className="mainLayout__navLinks">
+            <ul>
+              <li>
+                <Link to="/adopta">Adopta</Link>
+              </li>
+              <li>
+                <Link to="/voluntarios">Voluntariado</Link>
+              </li>
+              <li>
+                <Link to="/dona">Dona</Link>
+              </li>
+              <li>
+                <Link to="/sobre-nosotros">Sobre nosotros</Link>
+              </li>
+            </ul>
+            <Button
+              buttonType={ButtonType.PRIMARY}
+              text="Login"
+              onClick={() => console.log('Adopt button clicked')}
+            />
+          </div>
+        </nav>
+      </header>
+      <main>
+        <Outlet />
+      </main>
+      <footer className="mainLayout__footer">
+        <div className="mainLayout__footerSocials">
+          <Link to="/facebook">
+            <img
+              src="https://img.icons8.com/ios-glyphs/30/000000/facebook-new.png"
+              alt="Facebook"
+            />
+          </Link>
+          <Link to="/instagram">
+            <img
+              src="https://img.icons8.com/ios-glyphs/30/000000/instagram-new.png"
+              alt="Instagram"
+            />
+          </Link>
+          <Link to="/twitter">
+            <img
+              src="https://img.icons8.com/ios-glyphs/30/000000/twitter.png"
+              alt="Twitter"
+            />
+          </Link>
+        </div>
+        <p className="mainLayout__copy">
+          &copy; 2024 Gatos Sin Hogar Gran Canaria
+        </p>
+      </footer>
+    </div>
+  );
+};
+
+export default MainLayout;
